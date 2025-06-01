c6e65999290d217331810083c920d87bba38b918
dooooolaa <158486746+dooooolaa@users.noreply.github.com>
2025-06-01 08:30:27 +0200

    8

--- src/pages/adhkar/AdhkarPage.tsx
+++ src/pages/adhkar/AdhkarPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { Repeat, Copy, RefreshCw, Volume2 } from 'lucide-react';
 import { copyToClipboard } from '../../lib/utils';
