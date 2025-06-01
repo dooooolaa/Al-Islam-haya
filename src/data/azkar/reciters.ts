8433c6984993137183bb69d763659f22e2256407
dooooolaa <158486746+dooooolaa@users.noreply.github.com>
2025-06-01 04:56:32 +0200

    Update Islamic content and reciters

--- src/data/azkar/reciters.ts
+++ src/data/azkar/reciters.ts
@@ -1,63 +1,30 @@
-export interface Reciter {
-  id: string;
-  name: string;
-  nameEn: string;
-  url: string;
-}
-
-export const reciters: Reciter[] = [
+export const reciters = [
   {
-    id: 'raad-kurdi',
-    name: 'رعد محمد الكردي',
-    nameEn: 'Raad Muhammad Al-Kurdi',
-    url: 'https://surahquran.com/quran-mp3-qari-54.html'
-  },
-  {
-    id: 'mishary-rashid',
+    id: 'alafasy',
     name: 'مشاري راشد العفاسي',
     nameEn: 'Mishary Rashid Alafasy',
-    url: 'https://surahquran.com/mp3/Alafasi/116.html'
+    url: 'https://surahquran.com/mp3/Alafasi/116.html',
+    image: 'https://i.imgur.com/example1.jpg' // Replace with actual image URL
   },
   {
-    id: 'abdurrahman-sudais',
-    name: 'عبد الرحمن السديس',
-    nameEn: 'Abdurrahman As-Sudais',
-    url: 'https://surahquran.com/mp3/Alsudaes/116.html'
-  },
-  {
-    id: 'saud-shuraim',
-    name: 'سعود الشريم',
-    nameEn: 'Saud Ash-Shuraim',
-    url: 'https://surahquran.com/mp3/Al-Shuraim/116.html'
-  },
-  {
-    id: 'maher-al-muaiqly',
-    name: 'ماهر المعيقلي',
-    nameEn: 'Maher Al-Muaiqly',
-    url: 'https://surahquran.com/mp3/maher/116.html'
-  },
-  {
-    id: 'abdul-basit',
-    name: 'عبد الباسط عبد الصمد',
-    nameEn: 'Abdul Basit Abdul Samad',
-    url: 'https://surahquran.com/mp3/Abdulbaset/116.html'
-  },
-  {
-    id: 'mahmoud-khalil',
-    name: 'محمود خليل الحصري',
-    nameEn: 'Mahmoud Khalil Al-Husary',
-    url: 'https://surahquran.com/mp3/Al-Hussary-Mujawwad/116.html'
-  },
-  {
-    id: 'muhammad-siddiq',
+    id: 'minshawi',
     name: 'محمد صديق المنشاوي',
-    nameEn: 'Muhammad Siddiq Al-Minshawi',
-    url: 'https://surahquran.com/mp3/Al-Minshawi_Mojawwad/116.html'
+    nameEn: 'Mohamed Siddiq Al-Minshawi',
+    url: 'https://surahquran.com/mp3/Al-Minshawi_Mojawwad/116.html',
+    image: 'https://i.imgur.com/example2.jpg' // Replace with actual image URL
   },
   {
-    id: 'saad-al-ghamdi',
+    id: 'sudais',
+    name: 'عبدالرحمن السديس',
+    nameEn: 'Abdurrahman As-Sudais',
+    url: 'https://server11.mp3quran.net/sds/001.mp3',
+    image: 'https://i.imgur.com/example3.jpg' // Replace with actual image URL
+  },
+  {
+    id: 'ghamdi',
     name: 'سعد الغامدي',
     nameEn: 'Saad Al-Ghamdi',
-    url: 'https://surahquran.com/mp3/Al-Ghamdi/116.html'
+    url: 'https://server7.mp3quran.net/s_gmd/001.mp3',
+    image: 'https://i.imgur.com/example4.jpg' // Replace with actual image URL
   }
-]; 
+];
