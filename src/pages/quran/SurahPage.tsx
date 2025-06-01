093b7b016164a162a9b1ef8205812eef947e6b19
dooooolaa <158486746+dooooolaa@users.noreply.github.com>
2025-06-01 20:47:52 +0200

    5

--- src/pages/quran/SurahPage.tsx
+++ src/pages/quran/SurahPage.tsx
@@ -61,7 +61,7 @@ const reciters: Reciter[] = [
   {
     id: 'Albanna_murattal',
     name: 'محمود علي البنا (مرتل)',
-    audioUrl: 'https://server8.mp3quran.net/bna',
+    audioUrl: 'https://server8.mp3quran.net/bna/',
     image: 'https://watanimg.elwatannews.com/image_archive/original_lower_quality/5551145621689862618.jpg',
     type: 'direct',
     fileExtension: '.mp3'
