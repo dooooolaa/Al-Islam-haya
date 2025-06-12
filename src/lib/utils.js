90218ab73ea512402398c03ac213780234404b4b
dooooolaa <158486746+dooooolaa@users.noreply.github.com>
2025-06-13 01:26:45 +0200

    fix

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function formatNumber(number) {
+  // Convert English numbers to Arabic numbers
+  const arabicNumbers = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
+  return number.toString().replace(/[0-9]/g, (digit) => arabicNumbers[parseInt(digit)]);
+}
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
@@ -52,3 +52,4 @@ module.exports = {
   },
   plugins: [],
 };
+
